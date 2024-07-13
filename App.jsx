2d8a3763b89a9ee25dd85979fc239cae6774f4a0
Dipak Samadhan Mundhe <146376270+dsmundhe@users.noreply.github.com>
2024-07-13 01:53:31 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Layout from './components/Layout'
+import Home from './components/Home'
+function App() {
+  return (
+    <>
+        <Layout/>
+       
+    </>
+  )
+}
+
+export default App
